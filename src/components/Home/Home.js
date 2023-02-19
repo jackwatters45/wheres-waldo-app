28467607638472de467ce7f95991168e76751c6a
Jack Watters <johnwatt@usc.edu>
2023-02-18 23:30:23 -0800

    Home page.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import LevelCards from '../utils/LevelCards';
+import styled from 'styled-components';
+import HomeNav from './HomeNav';
+import Footer from '../utils/Footer';
+
+const HomeContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+`;
+
+const StyledLevelCards = styled(LevelCards)`
+  & img {
+    height: 320px;
+  }
+`;
+
+const Home = (props) => {
+  return (
+    <HomeContainer>
+      <HomeNav />
+      <StyledLevelCards page={'play'} />
+      <Footer />
+    </HomeContainer>
+  );
+};
+
+export default Home;
---
+++ src/components/Home/HomeNav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Title from '../utils/Title';
+
+const LeaderBoard = styled(Link)`
+  grid-column: 2;
+  justify-self: flex-end;
+  align-self: center;
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+const StyledNav = styled.nav`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  padding: 50px;
+`;
+
+const HomeNav = () => (
+  <StyledNav>
+    <Title />
+    <LeaderBoard to="/leaderboard/rocket">Leaderboard</LeaderBoard>
+  </StyledNav>
+);
+
+export default HomeNav;
